1d27c7c58a76f899012fad7b0c189821d6735d61
muhammad-hamza <Muh-Hamza-99>
2022-06-25 08:54:57 +0300

    amazon-scraper: Modification to url constructing function to account for pagination

--- amazon-scraper/main.py
+++ amazon-scraper/main.py
@@ -7,7 +7,9 @@ driver = webdriver.Firefox()
 def get_url(search_term):
     template = "https://www.amazon.com/s?k={}"
     search_term = search_term.replace(" ", "+")
-    return template.format(search_term)
+    url = template.format(search_term)
+    url += "&page={}"
+    return url
 
 url = get_url("monitor")
 driver.get(url)
